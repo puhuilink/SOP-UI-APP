fee597c9176656514491ada3db6706cd6ed69b74
xiaoasuna <asuna1542237464@163.com>
2022-07-14 15:31:16 +0800

    芋道移动端代码巨大变更

---
+++ pages/info/info.vue
@@ -0,0 +1,131 @@
+<template>
+	<view class="container">
+   <u-navbar
+            title="消息"
+            @rightClick="rightClick"
+            :autoBack="true"
+           rightIcon="search"
+        > 
+        </u-navbar>
+        <view class="division"></view>
+        <u-search shape="round"></u-search>
+           <view class="division"></view>
+        <u-sticky bgColor="#fff">
+    <u-tabs :list="list1"></u-tabs>
+     <view class="division"></view>
+      <u-list>
+        <u-list-item
+			  v-for="(item,index) in listData" :key="index" 
+			>
+      <u-col span="12" >
+        	<view class="time"> {{item.published_at }} </view>
+      </u-col>
+     
+    <u-cell
+     :title="item.title" 
+     :label="item.author_name"  
+    >  
+   	<!-- <u-icon slot="icon"  name="search" class="style"></u-icon> -->
+     <template slot="value">
+			<span class="right-top">{{item.id}}</span>
+      	<span class="right-bottom">{{item.name}}</span>
+		</template></u-cell>
+ 
+
+        </u-list-item>  
+          </u-list>
+  </u-sticky>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+        	type:"warning",
+			value:100,
+  list1: [{
+                    name: '已读',
+                }, {
+                    name: '未读',
+                }],
+                 listData: [{
+                        id: "S20220531199",                       
+                        title: "01-财务账号问题",
+                        author_name: "二线人员王明已处理",
+                        name: "记录人高得",
+                        published_at: "2022-07-22 08:19"
+                    },
+                    {
+                        id: "S20220531199",                       
+                        title: "02-财务账号问题",
+                        author_name: "二线人员王明已处理",
+                        name: "记录人李雷",
+                        published_at: "2022-07-23 08:19"
+                    },
+                    {
+                        id: "S20220531199",                       
+                        title: "03-财务账号问题",
+                        author_name: "二线人员王明已处理",
+                        name: "记录人王露",
+                        published_at: "2022-07-24 08:19"
+                    },
+                    {
+                         id: "S20220531199",                       
+                        title: "04-财务账号问题",
+                        author_name: "二线人员王明已处理",
+                        name: "记录人高得",
+                        published_at: "2022-07-25 08:19"
+                    }
+                ],
+			};
+		},
+		methods: {
+         rightClick() {
+            console.log('rightClick');
+        },
+        leftClick() {
+            console.log('leftClick');
+        }
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	.container {
+		padding-left: 0px;
+		padding-right: 0px;
+		font-size: 12px;
+	}
+	.container {
+		padding-bottom: 80px;
+	}
+  .division {
+    height: 10px;
+  }
+// .style{
+//   position:absolute;
+//    top:30px ;
+//   left:0px;
+// }
+     .right-top{
+      margin-top:-20px;
+      margin-right: -70px;
+     }
+     .right-bottom{
+       margin-bottom:-30px;
+height: 14px;
+font-size: 14px;
+color: #878B8F;
+     }
+     .time{
+      margin-top: 10px;
+      text-align: center;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #AFB1B5;
+line-height: 17px;
+     }
+</style>
---
+++ pages/info/test.vue
@@ -0,0 +1,121 @@
+<template>
+    <view class="content">
+        <active-form v-model="fields" num></active-form>
+
+        <view class="subform" @click="sub">提交表单</view>
+    </view>
+</template>
+
+<script>
+import ActiveForm from "@/components/active-form/active-form";
+
+export default {
+    components: {
+        ActiveForm,
+    },
+    data() {
+        return {
+		 "fields": [{
+    "__config__": {
+      "label": "记录人",
+      "labelWidth": null,
+      "showLabel": true,
+      "changeTag": true,
+      "tag": "el-input",
+      "tagIcon": "listicon_03",
+      "defaultValue": "admin",
+      "required": true,
+      "layout": "defaultData",
+      "span": 24,
+      "show": false,
+      "fixedType": "username",
+      "document": "https://element.eleme.cn/#/zh-CN/component/input",
+      "regList": [],
+      "formId": 103,
+      "renderKey": "1031657783621672"
+    },
+    "__slot__": {
+      "prepend": "",
+      "append": ""
+    },
+    "placeholder": "请输入记录人",
+    "style": {
+      "width": "100%"
+    },
+    "clearable": true,
+    "prefix-icon": "",
+    "suffix-icon": "",
+    "maxlength": null,
+    "show-word-limit": false,
+    "readonly": false,
+    "disabled": false,
+    "tableColumn": "user_name",
+    "__vModel__": "field103"
+  }, {
+    "__config__": {
+      "label": "工单编号",
+      "labelWidth": null,
+      "showLabel": true,
+      "changeTag": true,
+      "tag": "el-input",
+      "tagIcon": "listicon_01",
+      "required": true,
+      "layout": "text",
+      "span": 24,
+      "show": false,
+      "document": "https://element.eleme.cn/#/zh-CN/component/input",
+      "value": "系统自动生成",
+      "formId": 104,
+      "renderKey": "1041657783622285"
+    },
+    "placeholder": "系统自动生成工单编号",
+    "style": {
+      "width": "100%"
+    },
+    "clearable": true,
+    "prefix-icon": "",
+    "suffix-icon": "",
+    "maxlength": null,
+    "show-word-limit": false,
+    "readonly": false,
+    "disabled": false,
+    "tableColumn": "work_order_id"
+  }],
+        };
+    },
+      mounted() {
+    window.addEventListener("message", (event) => {
+      const data = event.data;
+      switch (data.type) {
+        case "formData":
+          // 业务逻辑
+          this.type = data.type;
+          this.fields = JSON.parse(data.data);
+          break;
+      }
+    });
+  },
+    methods: {
+        // 提交表单
+        sub() {
+            this.$refs.activeForm.$vervify(this.formData); //表单校验 成功会继续往下走 失败抛出异常
+            const res = this.$refs.activeForm.$submitForm(this.formData); //校验成功 获取表单值
+            console.log('表单对象 :>> ', res);
+        },
+    },
+};
+</script>
+
+<style lang="less">
+
+    .subform {
+       display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+        margin: 30rpx;
+        padding: 20rpx 60rpx;
+        border-radius: 18rpx;
+        background-color: bisque;
+    }
+</style>
