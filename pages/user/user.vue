8dff10a719d3b438b578eaca5f76a0f62cd72035
jiayunzhe <jiayunzhe@scsc.tech>
2022-07-29 15:03:56 +0800

    修改icon

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -3,103 +3,138 @@
     <view class="user-header">
       <view class="user-info" @click="loginOrJump('/pages/profile/profile')">
         <u-avatar size="80" :src="userInfo.avatar"></u-avatar>
-        <text class="nick-name">{{ hasLogin ? userInfo.nickname || '游客' : '登录/注册' }}</text>
+        <text class="nick-name">{{
+          hasLogin ? userInfo.nickname || "游客" : "登录/注册"
+        }}</text>
       </view>
     </view>
 
     <u-gap height="10" bgColor="#f3f3f3"></u-gap>
-      <u-notice-bar :text="noticeList" mode="closable" speed="100"  ></u-notice-bar>
-        <u-gap height="10" bgColor="#f3f3f3"></u-gap>
-     
+    <u-notice-bar :text="noticeList" mode="closable" speed="100"></u-notice-bar>
+    <u-gap height="10" bgColor="#f3f3f3"></u-gap>
     
-         <u-cell-group class="fun-list">
-           <view class="order-header">
-        <text class="order-title">我的工单</text>
-      </view> 
-      <u-cell class="fun-item" :border="false" icon="clock" title="我的待办" isLink url="/pages/list/Todolist"></u-cell>
-      <u-cell class="fun-item" :border="false" icon="checkmark-circle" title="我的已办" isLink></u-cell>
-      <u-cell class="fun-item" :border="false" icon="email" title="我管理的" isLink></u-cell>
-      <u-cell class="fun-item" :border="false" icon="chat" title="消息中心" @click="loginOrJump('/pages/address/list')" isLink></u-cell>
-    </u-cell-group>
-   <fui-card radius="5%" >
-				<view class="fui-card__title">
-				 宝剑锋从磨砺出，梅花香自苦寒来
-				</view>
-			</fui-card>
+    <view class="work-order">
+      <view class="work-order-tit">我的工单</view>
+      <view class="work-order-item">
+        <view class="work-order-item-left" @click="loginOrJump('/pages/list/Todolist')">
+          <img
+            src="/static/images/icon/user/wait-done.png"
+            class="work-order-item-icon"
+          />
+          <view class="work-order-item-tit">我的待办</view>
+        </view>
+        <u-icon name="arrow-right" />
+      </view>
+      <view class="work-order-item">
+        <view class="work-order-item-left">
+          <img
+            src="/static/images/icon/user/done.png"
+            class="work-order-item-icon"
+          />
+          <view class="work-order-item-tit">我的已办</view>
+        </view>
+        <u-icon name="arrow-right" />
+      </view>
+      <view class="work-order-item">
+        <view class="work-order-item-left">
+          <img
+            src="/static/images/icon/user/administration.png"
+            class="work-order-item-icon"
+          />
+          <view class="work-order-item-tit">我管理的</view>
+        </view>
+        <u-icon name="arrow-right" />
+      </view>
+      <view class="work-order-item" @click="loginOrJump('/pages/address/list')">
+        <view class="work-order-item-left">
+          <img
+            src="/static/images/icon/user/message.png"
+            class="work-order-item-icon"
+          />
+          <view class="work-order-item-tit">消息中心</view>
+        </view>
+        <u-icon name="arrow-right" />
+      </view>
+    </view>
 
     <view v-if="hasLogin" class="logout-btn">
-      <u-button type="error" color="#ea322b" text="退出登录" @click="logout"></u-button>
+      <u-button
+        type="error"
+        color="#ea322b"
+        text="退出登录"
+        @click="logout"
+      ></u-button>
     </view>
   </view>
 </template>
 
 <script>
-	import { getToDoList } from '@/api/list.js'
+import { getToDoList } from "@/api/list.js";
 export default {
   data() {
     return {
-      noticeList:'您有一个新的工单待接收，请及时处理',
+      noticeList: "您有一个新的工单待接收，请及时处理",
       orderStatusList: [
-        { icon: 'rmb-circle', title: '待支付' },
-        { icon: 'car', title: '代发货' },
-        { icon: 'order', title: '待收货' },
-        { icon: 'integral', title: '已完成' }
+        { icon: "rmb-circle", title: "待支付" },
+        { icon: "car", title: "代发货" },
+        { icon: "order", title: "待收货" },
+        { icon: "integral", title: "已完成" },
       ],
-			form:{
-				pageNo:1,
-				pageSize:10
-			},
+      form: {
+        pageNo: 1,
+        pageSize: 10,
+      },
       statList: [
-        { value: '0', title: '我的收藏' },
-        { value: '0', title: '我的消息' },
-        { value: '0', title: '我的足迹' }
-      ]
-    }
+        { value: "0", title: "我的收藏" },
+        { value: "0", title: "我的消息" },
+        { value: "0", title: "我的足迹" },
+      ],
+    };
   },
   onLoad() {
-    if (this.hasLogin){
-      this.$store.dispatch('ObtainUserInfo')
+    if (this.hasLogin) {
+      this.$store.dispatch("ObtainUserInfo");
     }
   },
-  created(){
-    this.getToDoList()
+  created() {
+    this.getToDoList();
   },
   methods: {
-    getToDoList(){
-      getToDoList(this.form).then(res=>{
-        console.log(res)
-      })
+    getToDoList() {
+      getToDoList(this.form).then((res) => {
+        console.log(res);
+      });
     },
     loginOrJump(pageUrl) {
       if (!this.hasLogin) {
-        uni.$u.route('/pages/login/social')
+        uni.$u.route("/pages/login/social");
       } else {
-        uni.$u.route(pageUrl)
+        uni.$u.route(pageUrl);
       }
     },
     logout() {
       uni.showModal({
-        title: '提示',
-        content: '您确定要退出登录吗',
-        success: res => {
+        title: "提示",
+        content: "您确定要退出登录吗",
+        success: (res) => {
           if (res.confirm) {
-            this.$store.dispatch('Logout')
+            this.$store.dispatch("Logout");
           } else if (res.cancel) {
             //console.log('用户点击取消')
           }
-        }
-      })
-    }
+        },
+      });
+    },
   },
   computed: {
     userInfo() {
-      return this.$store.getters.userInfo
+      return this.$store.getters.userInfo;
     },
     hasLogin() {
-      return this.$store.getters.hasLogin
-    }
-  }
-}
+      return this.$store.getters.hasLogin;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -116,57 +151,48 @@ export default {
   }
 }
 
-.order-header {
-  @include flex-space-between;
-  padding: 20rpx 30rpx;
-  border-bottom: $custom-border-style;
+.work-order {
+  width: 719rpx;
+  background: #ffffff;
+  border-radius: 21rpx;
+  margin: 0 auto;
 
-  .order-title {
-    width: 60px;
-height: 21px;
-font-size: 15px;
-font-family: PingFangSC-Semibold, PingFang SC;
-font-weight: 600;
-color: #333333;
-line-height: 21px;
+  .work-order-tit {
+    line-height: 102rpx;
+    padding: 0 30rpx;
+    font-size: 31rpx;
+    font-family: PingFangSC-Semibold, PingFang SC;
+    font-weight: 600;
+    color: #333333;
   }
-  .see-all {
-    height: 40rpx;
-    @include flex-right;
-    color: #666666;
-    font-size: 26rpx;
-  }
-}
 
-.order-status-box {
-  padding: 40rpx 0;
-}
+  .work-order-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 92rpx;
+    padding: 0 30rpx;
+    font-size: 29rpx;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: #000000;
 
-.stat-box {
-  padding: 20rpx 0;
-}
+    .work-order-item-left {
+      display: flex;
+      .work-order-item-tit {
+        margin-left: 15rpx;
+      }
+    }
 
-.grid-title {
-  line-height: 50rpx;
-  font-size: 26rpx;
-}
+    .work-order-item-icon {
+      width: 38rpx;
+      height: 38rpx;
+      display: block;
+    }
 
-.grid-value {
-  line-height: 50rpx;
-  font-size: 36rpx;
-  font-weight: 700;
-  color: #2b85e4;
-}
-
-.fun-list {
-  .fun-item {
-font-size: 14px;
-font-weight: 400;
-color: #000000;
-line-height: 14px;
-    padding-top: 10rpx;
-    padding-bottom: 10rpx;
-    border-bottom: $custom-border-style;
+    .u-icon {
+      color: #000000 !important;
+    }
   }
 }
 
