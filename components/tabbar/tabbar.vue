1eb67c922ccc0195e7fcb6dc3e09bbedb55609f8
jiayunzhe <jiayunzhe@scsc.tech>
2022-08-18 12:30:14 +0800

    服务热线功能完善

--- components/tabbar/tabbar.vue
+++ components/tabbar/tabbar.vue
@@ -1,6 +1,6 @@
 <template>
   <u-tabbar
-    :value="tabarList.findIndex((item) => item.name === indexBar)"
+    :value="tabarList.findIndex((item) => item.name === indexBar2)"
     :fixed="true"
     :placeholder="false"
     :safeAreaInsetBottom="false"
@@ -9,8 +9,21 @@
     <block v-for="(item, index) in tabarList" :key="index">
       <u-tabbar-item
         :text="item.text"
-        :icon="item.name === indexBar ? item.selectedIconPath : item.iconPath"
+        :icon="item.name === indexBar2 ? item.selectedIconPath : item.iconPath"
       ></u-tabbar-item>
+      <!-- 拨打电话 -->
+      <u-overlay :opacity="0.3" :show="!!(indexBar2 === 'serviceTel')">
+        <view class="tel-Box">
+          <view class="tel">
+            <text>客服电话</text>
+            <text>400-676-5885</text>
+            <view class="tel-btn">
+              <view @click="callTel">取消</view>
+              <view @click="callTel(item.tel)">拨打</view>
+            </view>
+          </view>
+        </view>
+      </u-overlay>
     </block>
   </u-tabbar>
 </template>
@@ -21,6 +34,8 @@ export default {
   props: ["indexBar"],
   data() {
     return {
+      notTelIndex: "index",
+      indexBar2: "index",
       tabbar: [
         {
           name: "index",
@@ -41,12 +56,12 @@ export default {
           selectedIconPath: "/static/images/tabbar/service-tel-active.png",
           tel: "400-676-5885",
         },
-        // {
-        //   name: "problems",
-        //   pagePath: "",
-        //   iconPath: "/static/images/tabbar/problems.png",
-        //   selectedIconPath: "/static/images/tabbar/problems-active.png",
-        // },
+        {
+          name: "problems",
+          pagePath: "",
+          iconPath: "/static/images/tabbar/problems.png",
+          selectedIconPath: "/static/images/tabbar/problems-active.png",
+        },
       ],
       tabarList: [],
     };
@@ -84,21 +99,76 @@ export default {
       deep: true,
       immediate: true,
     },
+    indexBar: {
+      handler: function (val) {
+        val && (this.indexBar2 = val);
+      },
+      deep: true,
+      immediate: true,
+    },
   },
   created() {},
   methods: {
     change(i) {
       let item = this.tabarList[i];
-      if (item.pagePath) {
+      if (item.tel) {
+        this.indexBar2 = "serviceTel";
+      } else if (item.pagePath) {
+        this.notTelIndex = item.name;
         uni.navigateTo({
           url: item.pagePath,
         });
-      } else if (item.tel) {
-        uni.makePhoneCall({ phoneNumber: item.tel });
       }
+    },
+    callTel(tel) {
+      tel && uni.makePhoneCall({ phoneNumber: tel });
+      this.indexBar2 = this.notTelIndex;
     },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.tel-Box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  .tel {
+    width: 638rpx;
+    background: #ffffff;
+    border-radius: 21rpx;
+    padding-top: 23rpx;
+    font-size: 38rpx;
+    font-family: PingFangSC-Medium, PingFang SC;
+    font-weight: 500;
+    color: #333333;
+    line-height: 52rpx;
+    text-align: center;
+    overflow: hidden;
+    text {
+      display: block;
+      margin-top: 23rpx;
+    }
+    .tel-btn {
+      margin-top: 65rpx;
+      background: #e0e0e0;
+      font-size: 33rpx;
+      font-family: MicrosoftYaHei;
+      color: #333333;
+      display: flex;
+      height: 87rpx;
+      view {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      view:last-child {
+        background: #007efd;
+        color: #ffffff;
+      }
+    }
+  }
+}
+</style>
