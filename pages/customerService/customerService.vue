f6d3fe71eeae33a1aac9327fad15c0c8b6ad444d
jiayunzhe <jiayunzhe@scsc.tech>
2022-08-18 16:33:59 +0800

    客服页面创建

---
+++ pages/customerService/customerService.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="container">
+    <Navbar title="客服" />
+
+    <u-gap height="110rpx" />
+    <Tabbar indexBar="customerService" />
+  </view>
+</template>
+
+<script>
+import Navbar from "@/components/navbar/navbar";
+import Tabbar from "@/components/tabbar/tabbar";
+export default {
+  name: "customerService",
+  components: {
+    Navbar,
+    Tabbar,
+  },
+  data() {
+    return {};
+  },
+  onLoad() {},
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
