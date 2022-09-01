960db680f0c95686cf9e527eb6f65611b91b7174
jiayunzhe <jiayunzhe@scsc.tech>
2022-09-01 15:12:25 +0800

    多选框

---
+++ components/work-order-form/checkboxs.vue
@@ -0,0 +1,145 @@
+<template>
+  <view class="container">
+    <view class="label">
+      <text class="colorRed" v-if="config.required">*</text>
+      <text>{{ `${config.label}：` }}</text>
+    </view>
+    <u--form :model="form" :rules="rules" ref="selects">
+      <u-form-item prop="val">
+        <u-checkbox-group
+          placement="row"
+          v-model="form.val"
+          @change="groupChange"
+        >
+          <u-checkbox
+            :customStyle="{ margin: '0 42rpx 30rpx 0' }"
+            v-for="(item, index) in checkboxlist"
+            :key="index"
+            :label="item.label"
+            :name="item.label"
+            @change="radioChange"
+            :disabled="config.disabled"
+          />
+        </u-checkbox-group>
+      </u-form-item>
+    </u--form>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "checkboxs",
+  props: {
+    value: {
+      type: Array | String,
+      default: () => {
+        return [];
+      },
+    },
+    config: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+  },
+  data() {
+    return {
+      form: {
+        val: [],
+      },
+      rules: {
+        val: [
+          {
+            type: "string",
+            required: false,
+            message: "",
+          },
+        ],
+      },
+      checkboxlist: [],
+    };
+  },
+  computed: {},
+  watch: {
+    value: {
+      handler: function (val) {
+        if (!val) return;
+        if (typeof val === "string") {
+          this.form.val = val.split();
+        } else if (val.length > 0) {
+          this.form.val = val;
+        }
+      },
+      immediate: true,
+    },
+    config: {
+      handler: function (val) {
+        if (val.required) {
+          this.rules.val[0].required = val.required;
+        }
+        if (val.label) {
+          this.rules.val[0].message = `请填写${val.label}`;
+        }
+        if (val.options) {
+          this.checkboxlist = val.options;
+        }
+      },
+      deep: true,
+      immediate: true,
+    },
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    groupChange(n) {
+      this.form.val = n;
+      this.$emit("input", n);
+    },
+    radioChange() {},
+    vervify(callBack) {
+      this.$refs.selects
+        .validate()
+        .then((res) => {
+          callBack(true);
+        })
+        .catch(() => {
+          callBack(false);
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  background: #fff;
+  font-size: 29rpx;
+  font-family: PingFangSC-Regular, PingFang SC;
+  font-weight: 400;
+  color: #333333;
+  line-height: 80rpx;
+  display: flex;
+}
+.label {
+  white-space: nowrap;
+  line-height: 70rpx;
+}
+.colorRed {
+  font-size: 25rpx;
+  font-weight: 400;
+  color: #e40a0a;
+  margin-right: 5rpx;
+}
+.u-icon {
+  margin-left: 20rpx;
+}
+.u-form {
+  margin-left: 20rpx;
+}
+.u-checkbox-group--row {
+  flex-wrap: wrap;
+  flex-shrink: initial;
+}
+</style>
