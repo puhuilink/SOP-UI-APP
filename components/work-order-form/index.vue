9dcf04ab696f1328458584da8c8d369901f45ce2
jiayunzhe <jiayunzhe@scsc.tech>
2022-09-01 14:26:20 +0800

    fix index

--- components/work-order-form/index.vue
+++ components/work-order-form/index.vue
@@ -1,16 +1,143 @@
 <template>
-  <view>
+  <view class="work-order-form">
     <block
       v-for="(item, index) in fields"
       :key="`${item.__config__.formId}_${index}`"
     >
-    
+      <!-- 单行输入框 -->
+      <view
+        class="item"
+        v-if="item.__config__.tag === 'el-input' && item.type != 'textarea'"
+      >
+        <Inputs
+          :ref="`form${index}`"
+          :config="{
+            label: item.__config__.label,
+            required: item.__config__.required,
+            disabled: item.disabled,
+            placeholder: item.placeholder,
+            clearable: item.clearable,
+            maxlength: item.maxlength,
+          }"
+          v-model="form[item.formDataType]"
+        />
+      </view>
+      <!-- 多行输入框 -->
+      <view
+        class="item"
+        v-else-if="item.__config__.tag == 'el-input' && item.type == 'textarea'"
+      >
+        <Textareas
+          :ref="`form${index}`"
+          :config="{
+            label: item.__config__.label,
+            required: item.__config__.required,
+            disabled: item.disabled,
+            placeholder: item.placeholder,
+            clearable: item.clearable,
+            maxlength: item.maxlength,
+          }"
+          v-model="form[item.formDataType]"
+        />
+      </view>
+      <!-- 下拉选择 -->
+      <view class="item" v-else-if="item.__config__.tag === 'el-select'">
+        <Selects
+          :ref="`form${index}`"
+          :config="{
+            label: item.__config__.label,
+            required: item.__config__.required,
+            disabled: item.disabled,
+            placeholder: item.placeholder,
+            options: item.__slot__.options,
+          }"
+          v-model="form[item.formDataType]"
+        />
+      </view>
+      <!-- 级联选择 -->
+      <view class="item" v-else-if="item.__config__.tag === 'el-cascader'">
+        <Cascader
+          :ref="`form${index}`"
+          :config="{
+            label: item.__config__.label,
+            required: item.__config__.required,
+            disabled: item.disabled,
+            placeholder: item.placeholder,
+            options: item.options,
+            separator: item.separator,
+          }"
+          v-model="form[item.formDataType]"
+        />
+      </view>
+      <!-- 单选 -->
+      <view class="item" v-else-if="item.__config__.tag === 'el-radio-group'">
+        <Radios
+          :ref="`form${index}`"
+          :config="{
+            label: item.__config__.label,
+            required: item.__config__.required,
+            disabled: item.disabled,
+            options: item.__slot__.options,
+          }"
+          v-model="form[item.formDataType]"
+        />
+      </view>
+      <!-- 多选 -->
+      <view
+        class="item"
+        v-else-if="item.__config__.tag === 'el-checkbox-group'"
+      >
+        <Checkboxs
+          :ref="`form${index}`"
+          :config="{
+            label: item.__config__.label,
+            required: item.__config__.required,
+            disabled: item.disabled,
+            options: item.__slot__.options,
+          }"
+          v-model="form[item.formDataType]"
+        />
+      </view>
+      <!-- 时间 -->
+      <view
+        class="item"
+        v-else-if="item.__config__.tag === 'el-time-picker'"
+      >
+        <Datetime
+          :ref="`form${index}`"
+          :config="{
+            label: item.__config__.label,
+            required: item.__config__.required,
+            disabled: item.disabled,
+            placeholder: item.placeholder,
+            options: item.options,
+            format: item.format,
+          }"
+          v-model="form[item.formDataType]"
+        />
+      </view>
     </block>
   </view>
 </template>
 
 <script>
+import Inputs from "./inputs.vue";
+import Textareas from "./textareas.vue";
+import Selects from "./selects.vue";
+import Cascader from "./cascader.vue";
+import Radios from "./radios.vue";
+import Checkboxs from "./checkboxs.vue";
+import Datetime from "./datetime.vue";
 export default {
+  components: {
+    Inputs,
+    Textareas,
+    Selects,
+    Cascader,
+    Radios,
+    Checkboxs,
+    Datetime,
+  },
   name: "workOrderForm",
   props: ["fields"],
   props: {
@@ -24,26 +151,64 @@ export default {
   data() {
     return {
       form: {},
+      refsArr: [],
     };
   },
   watch: {
     fields: {
       handler: function (val) {
+        console.log(val);
         this.form = val.reduce((obj, item) => {
           if (item.formDataType) {
             obj[item.formDataType] = item.__config__.defaultValue;
           }
           return obj;
         }, {});
-        console.log(this.form);
       },
       deep: true,
       immediate: true,
     },
   },
   computed: {},
-  methods: {},
+  methods: {
+    vervify(callBack) {
+      let adopt = [];
+      let refsArr = Object.keys(this.$refs);
+      refsArr.map(
+        (item) =>
+          this.$refs[item] &&
+          this.$refs[item][0] &&
+          this.$refs[item][0].vervify((is) => {
+            adopt.push(is);
+            if (adopt.length === refsArr.length) {
+              if (adopt.includes(false)) {
+                callBack(false);
+              } else {
+                callBack(this.form);
+              }
+            }
+          })
+      );
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.work-order-form {
+  margin: 22rpx auto 22rpx;
+  width: 719rpx;
+  background: #ffffff;
+  border-radius: 21rpx;
+  min-height: 100rpx;
+  overflow: hidden;
+  padding: 20rpx 24rpx;
+  box-sizing: border-box;
+
+  .item {
+    min-height: 100rpx;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
